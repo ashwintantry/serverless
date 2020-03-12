6a8c41085d049e030051207b817404a460be5eaf
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-12 20:16:11 +0530

    Update serverlessInfra.tf

--- serverlessInfra.tf
+++ serverlessInfra.tf
@@ -75,6 +75,7 @@ resource "aws_api_gateway_integration" "integration" {
 }
 
 resource "aws_api_gateway_deployment" "example_serverless_deploy" {
+  depends_on = ["aws_api_gateway_integration.integration"]
   stage_name  = "prod"
   rest_api_id = "${aws_api_gateway_rest_api.example_serverless_api.id}"
 }
