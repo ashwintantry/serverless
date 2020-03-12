4ae94b338d093c4858cfd5ec41009cde8ab53fd0
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-12 18:26:23 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "example-serverless-tan3-terraform-state-s3"
-    key            = "serverless/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
