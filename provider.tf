28e6fbeaa808b06dc69782765dd4f720871aae25
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-12 20:57:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region  = "ap-south-1"
-  access_key = "AKIA3DQK44NGG7PNSYIR"
-  secret_key = "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"
+  skip_credentials_validation = TRUE
   version = "2.15.0"
 }
