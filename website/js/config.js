c72484269913cb288ec9ea5b11ea5d72a4f376b6
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-13 12:19:15 +0530

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'temp_userPoolId',
+        userPoolClientId: 'temp_userPoolClientId',
+        region: 'ap-south-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'temp_invokeUrl'
     }
 };
