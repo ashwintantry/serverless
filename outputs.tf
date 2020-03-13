69a21b99b2659bf1c3b720c49a883acde4ac9119
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-13 10:33:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,11 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.tan3_test_serverless_s3.bucket
+  value = "aws_s3_bucket.tan3_test_serverless_s3.bucket"
 }
 output "s3_bucket_endpoint_name" {
-  value = aws_s3_bucket.tan3_test_serverless_s3.website_endpoint
+  value = "aws_s3_bucket.tan3_test_serverless_s3.website_endpoint"
+}
+output "api_http" {
+  value = "https://${aws_api_gateway_rest_api.example_serverless_api.id}.execute-api.ap-south-1.amazonaws.com/${aws_api_gateway_deployment.example_serverless_deploy.stage_name}"
 }
 output "curl" {
   value = "curl -H 'Content-Type: application/json' -X POST -d '{\"name\": \"Daniel\"}' https://${aws_api_gateway_rest_api.example_serverless_api.id}.execute-api.ap-south-1.amazonaws.com/${aws_api_gateway_deployment.example_serverless_deploy.stage_name}"
