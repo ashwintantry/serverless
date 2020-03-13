4a1e6f4d7830a721c0e1e0a51b6af983d12e3bce
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-13 12:27:29 +0530

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'temp_userPoolId',
-        userPoolClientId: 'temp_userPoolClientId',
+        userPoolId: temp_userPoolId,
+        userPoolClientId: temp_userPoolClientId,
         region: 'ap-south-1'
     },
     api: {
-        invokeUrl: 'temp_invokeUrl'
+        invokeUrl: temp_invokeUrl
     }
 };
