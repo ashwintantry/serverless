b2ea9690fde53c1a07bd1dd184da5f7b5e2ba52a
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-16 18:33:53 +0530

    Update serverlessInfra.tf

--- serverlessInfra.tf
+++ serverlessInfra.tf
@@ -72,7 +72,7 @@ resource "aws_api_gateway_method_response" "example_serverless_api_method_respon
     http_method   = "${aws_api_gateway_method.example_serverless_api_method.http_method}"
     status_code   = "200"
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+        "method.response.header.Access-Control-Allow-Origin" = true
     }
     depends_on = ["aws_api_gateway_method.example_serverless_api_method"]
 }
