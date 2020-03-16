e3aa36d44d17fc3124a70d798fb0b6cd30e42f4c
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-16 18:39:10 +0530

    Update serverlessInfra.tf

--- serverlessInfra.tf
+++ serverlessInfra.tf
@@ -91,7 +91,7 @@ resource "aws_lambda_permission" "lambda_permission" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda_serverless.function_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:ap-south-1:868707139949 :${aws_api_gateway_rest_api.example_serverless_api.id}/*/${aws_api_gateway_method.example_serverless_api_method.http_method}${aws_api_gateway_resource.example_serverless_api_resource.path}"
+  source_arn = "arn:aws:execute-api:ap-south-1:868707139949:${aws_api_gateway_rest_api.example_serverless_api.id}/*/${aws_api_gateway_method.example_serverless_api_method.http_method}${aws_api_gateway_resource.example_serverless_api_resource.path}"
 }
 
 resource "aws_api_gateway_integration_response" "integration_response" {
