4061be3cd975c15c99ad54e8ef21045736d3dcf7
ashwintantry <62090247+ashwintantry@users.noreply.github.com>
2020-03-16 18:31:26 +0530

    Update serverlessInfra.tf

--- serverlessInfra.tf
+++ serverlessInfra.tf
@@ -72,7 +72,7 @@ resource "aws_api_gateway_method_response" "example_serverless_api_method_respon
     http_method   = "${aws_api_gateway_method.example_serverless_api_method.http_method}"
     status_code   = "200"
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
     }
     depends_on = ["aws_api_gateway_method.example_serverless_api_method"]
 }
@@ -84,6 +84,14 @@ resource "aws_api_gateway_integration" "integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.lambda_serverless.invoke_arn}"
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda_serverless.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:ap-south-1:868707139949 :${aws_api_gateway_rest_api.example_serverless_api.id}/*/${aws_api_gateway_method.example_serverless_api_method.http_method}${aws_api_gateway_resource.example_serverless_api_resource.path}"
 }
 
 resource "aws_api_gateway_integration_response" "integration_response" {
